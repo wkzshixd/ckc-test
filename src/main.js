d7204efb8e1e9a4b37b4f3983759d2b638d5aed3
Yri Kiknadze <wkzshixd@gmail.com>
2023-12-07 18:15:22 +0300

    commiting changes - navbar, main banner, about section, contact section is done. estate banner is not working properly, map section and footer is on todays agenda

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('hllo!')
+document.addEventListener('DOMContentLoaded', function() {
+    let links = document.querySelectorAll('.nav-btn');
+    
+    links.forEach(function(link) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let elementId = this.getAttribute('href').substring(1);
+        let element = document.getElementById(elementId);
+
+        if (element) {
+          let navbarHeight = document.querySelector('.nav').offsetHeight;
+          let elementOffset = element.offsetTop - navbarHeight - 65;
+          window.scrollTo({
+            top: elementOffset,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });

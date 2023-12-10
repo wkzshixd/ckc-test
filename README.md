9509a1165549888bfda94123829c32790f46ecbe
Yri Kiknadze <wkzshixd@gmail.com>
2023-12-10 18:33:35 +0300

    last commit - Project is officially done.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+**github pages:** https://wkzshixd.github.io/ckc-test/src/index.html
+
 - **04.12.2023**
   Start of the project
 - **05.12.2023**
@@ -5,6 +7,21 @@
 - **06.12.2023**
   Completion of about section, contact section. Got some difficulties with estate banner, so i'm planning to finish it in the last order, after all the rest is done. (aprox. time usage - 5-6~ hours)
 - **07.12.2023**
-  I did everything, except estate banner (aprox. time usage 6~ hours)
+  I did everything, except estate banner (aprox. time usage - 6~ hours)
 - **08.12.2023**
-  Project completed - estate banner is done, that was a real challenge. Still have some minor issues that i will fix in the next couple days. (aprox. time usage 10~ hours)
+  Estate banner is done, that was a real challenge. Still have some minor issues that i will fix in the next couple days. (aprox. time usage - 10~ hours)
+- **09-10.12.2023**
+  Project completed. Fixed all the issues, added navbar animations with throttle, some buttons hovers, made a full-working slider on main banner (aprox. time usage - 4~ hours)
+
+- **04.12.2023**
+  Начало проекта
+- **05.12.2023**
+  Первоначальный коммит и завершение навигационной панели и главного баннера. Начало разработки баннера недвижимости (приблизительное затраченное время - 5~ часов)
+- **06.12.2023**
+  Доработка раздела «О компании», раздела «Контакты». Возникли трудности с баннером недвижимости, поэтому планирую закончить его в последнюю очередь, после того, как все остальное будет сделано. (приблизительное затраченное время - 5-6~ часов)
+- **07.12.2023**
+  Я сделал все, кроме баннера недвижимости (приблизительное затраченное время - 6~ часов)
+- **08.12.2023**
+  Баннер недвижимости готов, это было настоящим испытанием. Еще остались небольшие проблемы, которые я исправлю в ближайшие пару дней. (приблизительное затраченное время - 10~ часов)
+- **09-10.12.2023**
+  Проект завершен. Исправлены все проблемы, добавлена анимация навигатора с дросселем, наведение некоторых кнопок, сделан полноценный слайдер на главном баннере (приблизительное затраченное время - 4~ часа)
--- src/main.js
+++ src/main.js
@@ -1,20 +1,105 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let links = document.querySelectorAll('.nav-btn');
-    
-    links.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        let elementId = this.getAttribute('href').substring(1);
-        let element = document.getElementById(elementId);
 
-        if (element) {
-          let navbarHeight = document.querySelector('.nav').offsetHeight;
-          let elementOffset = element.offsetTop - navbarHeight - 65;
-          window.scrollTo({
-            top: elementOffset,
-            behavior: 'smooth'
-          });
-        }
-      });
+  
+  let swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+      renderBullet: function (index, className) {
+        return '<div class="' + className + '" src="../public/svg/slider.svg"></div>';
+      },
+    },
+  });
+
+
+    // Получение всех кнопок навбара
+  let links = document.querySelectorAll('.nav-btn');
+  
+  links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      let elementId = this.getAttribute('href').substring(1);
+      let element = document.getElementById(elementId);
+      if (element) {
+        let navbarHeight = document.querySelector('.nav').offsetHeight;
+        let elementOffset = element.offsetTop - navbarHeight;
+        window.scrollTo({
+          top: elementOffset,
+          behavior: 'smooth'
+        });
+      }
+    }); 
+  });
+
+  // Получение всех секций на странице
+  const sections = document.querySelectorAll('.section');
+  console.log(sections)
+
+
+  // Функция для определения текущей видимой секции
+  function getCurrentSection() {
+    let currentSection = sections[0];
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        currentSection = section;
+      }
+    });
+    console.log(currentSection)
+    return currentSection;
+  }
+
+  // Изначальное удаление класса aktive
+  links.forEach(link => {
+    link.getAttribute('href') != '#main-banner' ? link.classList.remove('active') : null;
+  })
+  // Функция для подсветки кнопки в навбаре
+  function highlightNavbarLink() {
+    const currentSection = getCurrentSection();
+    links.forEach(link => {
+        link.classList.remove('active');
+      const sectionId = link.getAttribute('href').substring(1);
+      if (currentSection.id === sectionId) {
+        link.classList.add('active');
+      }
+    });
+  }
+
+  // Trottle
+  function throttle(func, limit) {
+    let inThrottle;
+    
+    return function() {
+      const context = this;
+      const args = arguments;
+      
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        
+        setTimeout(() => {
+          inThrottle = false;
+        }, limit);
+      }
+    };
+  }
+  
+    // Применение Trottle для HighlightNavbarLink
+  let highlightNavbarLinkThrottled = throttle(highlightNavbarLink, 140)
+
+    // Обработчик прокрутки страницы
+  window.addEventListener('scroll', () => {
+    highlightNavbarLinkThrottled();
+  });
+
+  // Обработчик нажатия на кнопки в навбаре
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      highlightNavbarLinkThrottled();
     });
   });
+});
+
+
+// Swiper
